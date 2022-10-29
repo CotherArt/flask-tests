26e0bd56721b35a67bdfe1f989249e13febbcd11
CotherArt <cother@protonmail.com>
2022-10-29 11:17:45 -0700

    host property added

--- basic-login/app.py
+++ basic-login/app.py
@@ -20,4 +20,5 @@ def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # To be accesible on the network, change host=your_local_ip
+    app.run(debug=True, port=5000, host='127.0.0.1')
